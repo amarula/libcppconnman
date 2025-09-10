b57867a8721cd8536d9e6831a1b473a0d51b49bc
Eduardo Gonzalez <eduardo.gonzalez@amarulasolutions.com>
2025-09-10 11:23:22 +0200

    gdbus_private.hpp: Fix style and modernize warnings
    Signed-off-by: Eduardo Gonzalez <eduardo.gonzalez@amarulasolutions.com>

--- src/dbus/gdbus_private.hpp
+++ src/dbus/gdbus_private.hpp
@@ -17,7 +17,8 @@ class EnumStringMap {
 
     constexpr explicit EnumStringMap(std::array<Pair, N> init) : data_{init} {}
 
-    [[nodiscard]] constexpr std::string_view to_string(Enum enum_value) const {
+    [[nodiscard]] constexpr auto to_string(Enum enum_value) const
+        -> std::string_view {
         for (auto&& [e, s] : data_) {
             if (e == enum_value) {
                 return s;
@@ -26,7 +27,8 @@ class EnumStringMap {
         throw std::runtime_error("Invalid enum value");
     }
 
-    [[nodiscard]] constexpr Enum from_string(std::string_view str) const {
+    [[nodiscard]] constexpr auto from_string(std::string_view str) const
+        -> Enum {
         for (auto&& [e, s] : data_) {
             if (s == str) {
                 return e;
@@ -40,17 +42,17 @@ class EnumStringMap {
 };
 
 template <typename T = std::string, std::size_t N = 0>
-auto as_to_vector(GVariant* array_s, const EnumStringMap<T, N>* enumMap =
+auto as_to_vector(GVariant* array_s, const EnumStringMap<T, N>* enum_map =
                                          nullptr) -> std::vector<T> {
-    GVariantIter strIter;
+    GVariantIter str_iter;
     std::vector<T> container;
     if (g_variant_is_of_type(array_s, G_VARIANT_TYPE("as")) != 0U) {
-        g_variant_iter_init(&strIter, array_s);
+        g_variant_iter_init(&str_iter, array_s);
         GVariant* item = nullptr;
-        while ((item = g_variant_iter_next_value(&strIter)) != nullptr) {
+        while ((item = g_variant_iter_next_value(&str_iter)) != nullptr) {
             const gchar* item_str = g_variant_get_string(item, nullptr);
             if constexpr (std::is_enum_v<T>) {
-                container.emplace_back(enumMap->from_string(item_str));
+                container.emplace_back(enum_map->from_string(item_str));
             } else {
                 container.emplace_back(item_str);
             }
