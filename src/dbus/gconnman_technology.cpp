6c021ca8321f96ce91c72e4502936b4a4da13de7
Eduardo Gonzalez <eduardo.gonzalez@amarulasolutions.com>
2026-05-12 16:30:04 +0200

    connman:technology: Add tethering methods
    Add tethering-properties setting methods for the technologies.
    Use unsigned int for storing the frequency.
    
    Signed-off-by: Eduardo Gonzalez <eduardo.gonzalez@amarulasolutions.com>

--- src/dbus/gconnman_technology.cpp
+++ src/dbus/gconnman_technology.cpp
@@ -34,6 +34,36 @@ void Technology::setPowered(bool powered, PropertiesSetCallback callback) {
                 &Technology::finishAsyncCall, data.release());
 }
 
+void Technology::setTethering(bool tethering, PropertiesSetCallback callback) {
+    auto data = prepareCallback(std::move(callback));
+    setProperty(TETHERING_STR,
+                g_variant_new_boolean(static_cast<gboolean>(tethering)),
+                nullptr, &Technology::finishAsyncCall, data.release());
+}
+
+void Technology::setTetheringIdentifier(const std::string& identifier,
+                                        PropertiesSetCallback callback) {
+    auto data = prepareCallback(std::move(callback));
+    setProperty(TETHERINGIDENTIFIER_STR,
+                g_variant_new_string(identifier.c_str()), nullptr,
+                &Technology::finishAsyncCall, data.release());
+}
+
+void Technology::setTetheringPassphrase(const std::string& passphrase,
+                                        PropertiesSetCallback callback) {
+    auto data = prepareCallback(std::move(callback));
+    setProperty(TETHERINGPASSPHRASE_STR,
+                g_variant_new_string(passphrase.c_str()), nullptr,
+                &Technology::finishAsyncCall, data.release());
+}
+
+void Technology::setTetheringFreq(const uint32_t frequency,
+                                  PropertiesSetCallback callback) {
+    auto data = prepareCallback(std::move(callback));
+    setProperty(TETHERINGFREQ_STR, g_variant_new_uint32(frequency), nullptr,
+                &Technology::finishAsyncCall, data.release());
+}
+
 void Technology::scan(PropertiesSetCallback callback) {
     auto data = prepareCallback(std::move(callback));
     callMethod(nullptr, SCAN_STR, nullptr, &Technology::finishAsyncCall,
@@ -54,6 +84,10 @@ void TechProperties::update(const gchar* key, GVariant* value) {
         tethering_ = g_variant_get_boolean(value) == 1U;
     } else if (g_strcmp0(key, TETHERINGFREQ_STR) == 0U) {
         tethering_freq_ = g_variant_get_int32(value);
+    } else if (g_strcmp0(key, TETHERINGIDENTIFIER_STR) == 0U) {
+        tethering_identifier_ = g_variant_get_string(value, nullptr);
+    } else if (g_strcmp0(key, TETHERINGPASSPHRASE_STR) == 0U) {
+        tethering_passphrase_ = g_variant_get_string(value, nullptr);
     } else {
         LCM_LOG("Unknown property for Technology: " << key << '\n');
     }
@@ -65,7 +99,13 @@ auto operator<<(std::ostream& ost, const TechProperties& obj) -> std::ostream& {
     ost << POWERED_STR << ": " << std::boolalpha << obj.powered_ << '\n';
     ost << CONNECTED_STR << ": " << std::boolalpha << obj.connected_ << '\n';
     ost << TETHERING_STR << ": " << std::boolalpha << obj.tethering_ << '\n';
-    ost << TETHERINGFREQ_STR << ": " << obj.tethering_freq_ << " \n";
+    if (obj.type_ == TechProperties::Type::Wifi) {
+        ost << TETHERINGIDENTIFIER_STR << ": " << obj.tethering_identifier_
+            << '\n';
+        ost << TETHERINGPASSPHRASE_STR << ": " << obj.tethering_passphrase_
+            << '\n';
+        ost << TETHERINGFREQ_STR << ": " << obj.tethering_freq_ << " \n";
+    }
     return ost;
 }
 
