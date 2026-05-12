3510b77fc59cc652809c89109e55584e2abe8dd6
Eduardo Gonzalez <eduardo.gonzalez@amarulasolutions.com>
2026-05-12 16:30:04 +0200

    connman:technology: Add tethering methods
    Add tethering-properties setting methods for the technologies.
    
    Signed-off-by: Eduardo Gonzalez <eduardo.gonzalez@amarulasolutions.com>

--- include/amarula/dbus/connman/gtechnology.hpp
+++ include/amarula/dbus/connman/gtechnology.hpp
@@ -29,6 +29,12 @@ struct TechProperties {
     [[nodiscard]] auto isConnected() const { return connected_; }
     [[nodiscard]] auto isTethering() const { return tethering_; }
     [[nodiscard]] auto getTetheringFreq() const { return tethering_freq_; }
+    [[nodiscard]] auto getTetheringIdentifier() const {
+        return tethering_identifier_;
+    }
+    [[nodiscard]] auto getTetheringPassphrase() const {
+        return tethering_passphrase_;
+    }
     friend auto operator<<(std::ostream& ostr,
                            const TechProperties& object) -> std::ostream&;
 
@@ -37,6 +43,8 @@ struct TechProperties {
     bool connected_ = false;
     bool tethering_ = false;
     std::string name_;
+    std::string tethering_identifier_;
+    std::string tethering_passphrase_;
     Type type_;
     int tethering_freq_{0};
 
@@ -54,6 +62,13 @@ class Technology : public DBusProxy<TechProperties> {
    public:
     using Properties = TechProperties;
     void setPowered(bool powered, PropertiesSetCallback callback = nullptr);
+    void setTethering(bool tethering, PropertiesSetCallback callback = nullptr);
+    void setTetheringIdentifier(const std::string& identifier,
+                                PropertiesSetCallback callback = nullptr);
+    void setTetheringPassphrase(const std::string& passphrase,
+                                PropertiesSetCallback callback = nullptr);
+    void setTetheringFreq(int frequency,
+                          PropertiesSetCallback callback = nullptr);
     void scan(PropertiesSetCallback callback = nullptr);
 
     friend class Manager;
